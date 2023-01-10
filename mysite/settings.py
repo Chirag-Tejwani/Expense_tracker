f9a6589fac4930a258122bb1197c202a9fd8bf0f
chi_tej <18ucs088@lnmiit.ac.in>
2023-01-11 01:35:14 +0530

    updated dependencies

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-r9r9$kwhg6j%%p1$jg5cd6@1%7c%u%si5ap88#9bmdk#-hir7b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
